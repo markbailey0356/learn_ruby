42bad17c336bee72f1c468efc0e649cde6ab511b
Mark Bailey <markbailey0356@gmail.com>
2018-10-03 19:23:44 +0930

    Complete 05_book_titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+  attr_reader :title
+  def title=(title)
+    @title = titleize(title)
+  end
+
+  private
+  def titleize(string)
+    string.split
+          .each_with_index
+          .map do |word, index|
+            if index != 0 && word.match(/^(and|over|the|of|an?|in)$/)
+              word
+            else
+              word.capitalize
+            end
+          end
+          .join(" ")
+  end
 end
