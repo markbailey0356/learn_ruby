df025937843de5c59753c83ab45785377a79c736
Mark Bailey <markbailey0356@gmail.com>
2018-10-03 18:24:09 +0930

    Complete 03_simon_says exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  Array.new(times, string).join(" ")
+end
+
+def start_of_word(string, number)
+  string[0,number]
+end
+
+def first_word(string)
+  string.split(" ").first
+end
+
+def titleize(string)
+  string.split
+        .each_with_index
+        .map do |word, index|
+          if index != 0 && word.match(/^(and|over|the|of|a)$/)
+            word
+          else
+            word.capitalize
+          end
+        end
+        .join(" ")
+end
